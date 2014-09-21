b940729894a8eab9fcc966506503bccb734a04b8
Nikita Porshnjakov <n.porshnjakov@gmail.com>
2014-09-21 16:32:20 +0300

    login/register forms validation js/css complete

--- js/custom/client-side.js
+++ js/custom/client-side.js
@@ -10,17 +10,41 @@ jQuery( document ).ready(function( $ ) {
 				minlength: 2,
 				pattern: '^[A-Za-z0-9]+$'
 			},
-			
-			emailadress: "required",																					
-			
-			password: { 
+
+			emailadress: "required",
+
+			reg_pw: {
 				required: true,
 				minlength: 6
 			},
 			password_again: {
-				equalTo: "#password"
-			}																						
-		}
+				required: true,
+				equalTo: "#reg_pw"
+			}
+		},
+		submitHandler: function() { alert("Submitted!") }
 	});
-	
+	// authentication validator
+	$( "#user_auth" ).validate({
+		rules: {
+			username: {
+				required: true,
+				minlength: 2,
+				pattern: '^[A-Za-z0-9]+$'
+			},
+
+			log_pw: {
+				required: true,
+				minlength: 6
+			}
+		},
+		submitHandler: function() { alert("Submitted!") }
+	});
+	$( "#auth_restore" ).validate({
+		rules: {
+			emailadress: "required"
+		},
+		submitHandler: function() { alert("Submitted!") }
+	});
+
 });
